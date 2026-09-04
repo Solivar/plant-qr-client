f9d7af02ba959876b1a0fd5eaedb8fb498e9a134
Ivars Solovjovs <solovjovs.ivars@gmail.com>
2026-09-04 17:02:26 +0300

    chore: eslint setup

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,9 @@
 import js from '@eslint/js';
-import eslintReact from '@eslint-react/eslint-plugin';
 import globals from 'globals';
+import reactDom from 'eslint-plugin-react-dom';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import reactX from 'eslint-plugin-react-x';
 import tseslint from 'typescript-eslint';
 import { defineConfig, globalIgnores } from 'eslint/config';
 
@@ -13,7 +14,8 @@ export default defineConfig([
     extends: [
       js.configs.recommended,
       tseslint.configs.recommendedTypeChecked,
-      eslintReact.configs['recommended-type-checked'],
+      reactX.configs['recommended-typescript'],
+      reactDom.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
